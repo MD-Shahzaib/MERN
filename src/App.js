5390b7ee0766441f3d9324b7dbce3bcbc96481c9
Muhammad Shahzaib <mdshahzaib36@gmail.com>
2023-05-04 02:29:32 -0700

    Create Simple frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
-
 function App() {
+  const [messageInput, setMessageInput] = useState('')
+  const sendMsg = () => {
+    console.log(messageInput);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container border mt-5 p-5">
+        <div className="messageContainer bg-secondary container text-white p-4">
+          <div className="messageLeft bg-primary rounded p-2 m-2 text-start">freind</div>
+          <div className="messageRight bg-primary rounded p-2 m-2 text-end">Me</div>
+        </div>
+        <div className='text-center bg-dark p-2'>
+          <input
+            type="text"
+            placeholder="Type a message"
+            className='p-2 mx-2'
+            value={messageInput}
+            onChange={e => setMessageInput(e.target.value)}
+          />
+          <div className="btn btn-primary" onClick={sendMsg}>Send</div>
+        </div>
+      </div>
+    </>
   );
 }
-
 export default App;
